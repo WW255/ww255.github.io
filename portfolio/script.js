936401991a894e06301419938f5e330520f6346c
WW255 <pablojuanoliveira009@gmail.com>
2026-05-02 18:32:39 -0300

    Changed layout to center, added icon. Just realized a problem but that's for later :D
    Lowkey used a *{ text-align: center !important; margin: 0 !important} so that may break things in the future

--- portfolio/script.js
+++ portfolio/script.js
@@ -18,3 +18,28 @@ for(let element of elements){
 		click_event(content)		
 	}
 }
+
+/* Stars */
+/* Given that A is the position of the first star, B is the middle of the screen,
+C is the angle -5, D is the position of any other star, solve for X
+A-B -> C
+D-B -> X */
+/* (D-B)*C = (A-B)*X */
+
+const factor = 5
+
+let stars = document.getElementById('stars');
+let first_star = stars.children[0]
+
+let body_rect = document.body.getBoundingClientRect()
+let half_of_screen = body_rect.width/2
+
+
+
+for(element of stars.children){
+	var rect = element.getBoundingClientRect() 
+	var relative_pos = rect.x - half_of_screen
+	var multiple = relative_pos*factor
+	var division = multiple/first_star.x
+	element.style.setProperty('--hover-angle',String(division)+"deg")
+}
