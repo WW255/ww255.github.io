09c64d150c8892cc89d4828e3b1d9473500a22b9
WW255 <pablojuanoliveira009@gmail.com>
2026-06-25 20:07:54 -0300

    box-sizing madness + wallpaper easter egg
    fixed the 3px tilt of the screen and added a new background

--- stiff android ui/index.js
+++ stiff android ui/index.js
@@ -1,9 +1,25 @@
+const main = document.getElementById("main")
 const el = document.getElementsByClassName("notification")[0]
 const y = el.getBoundingClientRect().y
+
+const url_1 = "wallpaper1.webp"
+const url_2 = "wallpaper2.png"
+
+var which = 0
 
 
 var listening = false
 var mouse_y = 0
+
+
+function change_wallpaper(){
+	which += 1
+	if(which == 2){
+		which = 0
+	}
+	l = [url_1, url_2]
+	main.style.backgroundImage = `url('assets/${l[which]}')`
+}
 
 el.addEventListener("mousedown", (event)=>{
 	listening = true
@@ -13,7 +29,7 @@ el.addEventListener("mousedown", (event)=>{
 window.addEventListener("mouseup", (event)=>{
 	if(listening){
 		if(mouse_y > event.clientY){
-			//
+			change_wallpaper()
 		}
 	}
 	listening = false
