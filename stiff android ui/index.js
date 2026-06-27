9920c433e14d15cbf26fbf91429481847b7a017f
WW255 <pablojuanoliveira009@gmail.com>
2026-06-27 18:26:49 -0300

    instagram screen size fix + new features for the android
    swipe left or right now

--- stiff android ui/index.js
+++ stiff android ui/index.js
@@ -1,14 +1,20 @@
-const main = document.getElementById("main")
-const el = document.getElementsByClassName("notification")[0]
-const y = el.getBoundingClientRect().y
+const screen = document.getElementById("screen")
+const content = document.getElementById("content")
+const notification = document.getElementsByClassName("notification")[0]
+
+
+let screen_width = screen.getBoundingClientRect().width
+const boundary = 0.30*screen_width
 
 const url_1 = "wallpaper1.webp"
 const url_2 = "wallpaper2.png"
 
 var which = 0
 
+var listening_left = false
+var listening_right = false
 
-var listening = false
+var mouse_x = false
 var mouse_y = 0
 
 
@@ -18,19 +24,48 @@ function change_wallpaper(){
 		which = 0
 	}
 	l = [url_1, url_2]
-	main.style.backgroundImage = `url('assets/${l[which]}')`
+	screen.style.backgroundImage = `url('assets/${l[which]}')`
 }
 
-el.addEventListener("mousedown", (event)=>{
-	listening = true
-	mouse_y = event.clientY
+function swipe_notification(difference){
+	if(difference > boundary){
+		notification.classList.add("swipe")
+	}
+	setTimeout(()=>{
+		notification.remove()
+	},1000);
+}
+
+content.addEventListener("mousedown", (event)=>{
+	content.classList.add("zoom")
+});
+
+content.addEventListener("mouseup", (event)=>{
+	content.classList.remove("zoom")
+});
+
+screen.addEventListener("mousedown", (event)=>{
+	listening_left = true
+	mouse_x = event.clientX
+	//mouse_y = event.clientY
+});
+
+notification.addEventListener("mousedown", (event)=>{
+	listening_right = true
+	mouse_x = event.clientX
+	//mouse_y = event.clientY
+	event.stopPropagation()
 });
 
 window.addEventListener("mouseup", (event)=>{
-	if(listening){
-		if(mouse_y > event.clientY){
-			change_wallpaper()
-		}
+	if(listening_left && mouse_x > event.clientX){
+		change_wallpaper()
 	}
-	listening = false
+	if(listening_right && mouse_x < event.clientX){
+		swipe_notification(event.clientX-mouse_x)
+	}
+	listening_left = false
+	listening_right = false
 });
+
+screen.style.opacity = "100"
